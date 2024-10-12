414b632c2143ac0158ff1ad26c77c317c96e941a
hamrouni-wajdi <hamrouniwajdi14@gmail.com>
2024-10-12 13:44:11 +0100

    user friendly type

--- models/pokemonModel.js
+++ models/pokemonModel.js
@@ -2,7 +2,7 @@ const db = require("../config/database");
 
 const PokemonModel = {
     async createPokemon(pokemon){
-        let {rows} = await db.query(`INSERT INTO pokemon(name,type,image,power,life) VALUES($1,$2,$3,$4,$5) RETURNING * `,[
+        let {rows} = await db.query(`INSERT INTO pokemon(name,type,image,power,life) VALUES($1,(SELECT id FROM pokemon_type WHERE name = $2),$3,$4,$5) RETURNING * `,[
             pokemon.name,pokemon.type,pokemon.image,pokemon.power,pokemon.life]) 
             return rows[0];
     }
